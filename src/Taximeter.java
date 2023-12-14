2ac9a5e53825db4d9cecf6b15b7df7571c72f73d
yunuSdet <yunuskulcu84@gmail.com>
2023-12-14 13:47:00 +0300

    Taximeter class is added

---
+++ src/Taximeter.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Taximeter {
+
+    public static void main(String[] args) {
+
+        Scanner input =new Scanner(System.in);
+
+        double startingAmount = 10;
+        double valueOfTaximeterForPerKm = 2.20;
+        double minimumValue = 20;
+
+        double km = 150;
+
+        double amount = startingAmount + valueOfTaximeterForPerKm*km;
+
+        if(amount<minimumValue) amount=minimumValue;
+
+        System.out.println("The amount for "+km+" km is :"+amount);
+
+
+
+    }
+}
