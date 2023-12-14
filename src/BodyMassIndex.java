628bac3854293ca20ee100e871e7933ce2beb5be
yunuSdet <yunuskulcu84@gmail.com>
2023-12-14 18:23:26 +0300

    BodyMassIndex file is added

---
+++ src/BodyMassIndex.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BodyMassIndex {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter your height (in meters):");
+        double height = input.nextDouble();
+
+        System.out.print("Please enter your weight: ");
+        double weight = input.nextDouble();
+
+        double BMI = weight/(Math.pow(height,2));
+        System.out.println("Your Body Mass Index (BMI) is: "+BMI);
+
+
+    }
+}
