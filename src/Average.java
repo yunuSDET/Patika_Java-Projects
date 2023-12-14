ac9d58b85cc0369c61ba0a8d28a92da417da6299
yunuSdet <yunuskulcu84@gmail.com>
2023-12-14 13:05:47 +0300

    first commit

---
+++ src/Average.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Average {
+
+    public static void main(String[] args) {
+
+        Scanner input =new Scanner(System.in);
+
+        double sum =0;
+
+
+        String[] lessons = {"Turkish","Math","Physics","Chemistry","History","Music"};
+
+        for (String eachLesson : lessons) {
+            System.out.println("Enter your "+eachLesson+" exam score: ");
+            sum+= input.nextInt();
+        }
+
+        System.out.println("The average of exam scores is "+(sum/lessons.length));
+
+
+
+
+
+
+
+    }
+}
