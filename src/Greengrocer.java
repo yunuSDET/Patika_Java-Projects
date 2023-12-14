61a3de7b633e04b28f26250c5c8a32329b677d7a
yunuSdet <yunuskulcu84@gmail.com>
2023-12-14 18:38:00 +0300

    Greengrocer class is added

---
+++ src/Greengrocer.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Greengrocer {
+
+    public static void main(String[] args) {
+
+        Scanner input =new Scanner(System.in);
+
+      Map<String,Double> products = new HashMap<>();
+      products.put("Apple",3.67);
+      products.put("Tomato",1.11);
+      products.put("Banana",0.95);
+      products.put("Eggplant",5.00);
+      products.put("Pear",2.14);
+        double sum = 0;
+
+        for (Map.Entry<String, Double> eachEntry : products.entrySet()) {
+
+            System.out.println("Please enter the weight  (kg) of "+eachEntry.getKey()+": ");
+            double eachKg = input.nextDouble();
+            sum+=eachKg*eachEntry.getValue();
+        }
+
+        System.out.println("Total amount is: "+sum+" ₺");
+
+
+    }
+}
