c114479b38a091821530ba8fa30daf04ec97ff33
yunuSdet <yunuskulcu84@gmail.com>
2023-12-20 00:45:50 +0300

    GuessNumber class is updated

--- src/Task_049_GuessNumber.java
+++ src/Task_049_GuessNumber.java
@@ -11,52 +11,41 @@ public class Task_049_GuessNumber {
         //int number = (int) (Math.random() * 100);
 
         Scanner input = new Scanner(System.in);
-        int right = 0;
-        int selected;
-        int[] wrong = new int[5];
-        boolean isWin = false;
-        boolean isWrong = false;
 
-        System.out.println(number);
-        while (right < 5) {
-            System.out.print("Lütfen tahmininizi giriniz : ");
-            selected = input.nextInt();
+        int guess;
 
-            if (selected < 0 || selected > 99) {
-                System.out.println("Lütfen 0-100 arasında bir değer giriniz.");
-                if (isWrong) {
-                    right++;
-                    System.out.println("Çok fazla hatalı giriş yaptınız. Kalan hak : " + (5 - right));
-                } else {
-                    isWrong = true;
-                    System.out.println("Bir daha hatalı girişinizde hakkınızdan düşülecektir.");
-                }
-                continue;
-            }
+        boolean quit=false;
+        int right=5;
 
-            if (selected == number) {
-                System.out.println("Tebrikler, doğru tahmin ! Tahmin ettiğini sayı : " + number);
-                isWin = true;
+        System.out.println("Guess the number: ");
+        guess =input.nextInt();
+
+        while (!quit){
+
+            if(right==0){
+                System.out.println("You couldn't find the number in 5 tries");
                 break;
-            } else {
-                System.out.println("Hatalı bir sayı girdiniz !");
-                if (selected > number) {
-                    System.out.println(selected + " sayısı, gizli sayıdan büyüktür.");
-                } else {
-                    System.out.println(selected + " sayısı, gizli sayıdan küçüktür.");
-                }
+            };
 
-                wrong[right++] = selected;
-                System.out.println("Kalan hakkı : " + (5 - right));
+
+            if(guess<number){
+                System.out.println("Increase your number: ");
+                guess =input.nextInt();
+            }else if(guess>number){
+                System.out.println("Decrease your number: ");
+                guess =input.nextInt();
+            }else{
+                System.out.println("You found the number!");
+                quit=true;
             }
+
+            right--;
         }
 
-        if (!isWin) {
-            System.out.println("Kaybettiniz ! ");
-            if (!isWrong) {
-                System.out.println("Tahminleriniz : " + Arrays.toString(wrong));
-            }
-        }
+
+
+
+
 
     }
 
