ff3252dc8992a5fee14285dd04b49521491675fe
yunuSdet <yunuskulcu84@gmail.com>
2023-12-20 12:48:48 +0300

    Zoo package is created

---
+++ src/Zoo/Diagram.java
@@ -0,0 +1,55 @@
+package Zoo;
+
+public class Diagram {
+
+}
+
+//abstract class Hayvan {
+//  - tür: string
+//  - ağırlık: int
+//  - yaş: int
+
+//    abstract getDosage(): float
+//    abstract getFeedSchedule(): string
+//}
+
+//class At : Hayvan {
+//        override getDosage(): float {
+//        // Atlar için dozaj hesaplama algoritması
+//        }
+//
+//        override getFeedSchedule(): string {
+//        // Atlar için yem verme zamanları
+//        }
+//        }
+//
+//class Kedigiller : Hayvan {
+//        override getDosage(): float {
+//        // Kedigilleri için dozaj hesaplama algoritması
+//        }
+//
+//        override getFeedSchedule(): string {
+//        // Kedigilleri için yem verme zamanları
+//        }
+//        }
+//
+//class Kemirgenler : Hayvan {
+//        override getDosage(): float {
+//        // Kemirgenler için dozaj hesaplama algoritması
+//        }
+//
+//        override getFeedSchedule(): string {
+//        // Kemirgenler için yem verme zamanları
+//        }
+//        }
+
+
+//        Hayvan "1" -- "*" At : atlar
+//        Hayvan "1" -- "*" Kedigiller : kedigilleri
+//        Hayvan "1" -- "*" Kemirgenler : kemirgenler
+//
+//        Hayvan::getDosage()
+//        Hayvan::getFeedSchedule()
+
+
+
